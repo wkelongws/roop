623410356907aa7674c2f0a191ee9c9d106b9bb8
Shuo Wang <shuow@nvidia.com>
2023-06-01 23:00:25 -0700

    play on selecting from multiple faces

--- core/analyser.py
+++ core/analyser.py
@@ -15,6 +15,13 @@ def get_face_analyser():
 def get_face(img_data):
     face = get_face_analyser().get(img_data)
     try:
-        return sorted(face, key=lambda x: x.bbox[0])[0]
+        # print(type(face))
+        # print(len(face))
+        # print(face[0].bbox)
+        # print(sorted(face, key=lambda x: x.bbox[0])[0])
+        if len(face)==1:
+            return sorted(face, key=lambda x: x.bbox[0])[0]
+        else:
+            return sorted(face, key=lambda x: x.bbox[0])[-1]
     except IndexError:
         return None
